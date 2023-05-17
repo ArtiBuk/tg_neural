769e3276eeb43265d8a41a170e86a4f4c9467a67
artibuk <artibuk1052@icloud.com>
2023-05-17 21:46:13 +0700

    fake_useragent_and_fix_cod

--- parser_google.py
+++ parser_google.py
@@ -25,21 +25,29 @@
 # search_url = search_yandex(user_query)
 # print("Открыта страница поиска:", search_url)
 
+
 import time
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import unquote, urlparse
+from urllib.parse import unquote_plus, urlparse
+import random
+from fake_useragent import UserAgent
+
 
 def search_google(query, limit):
     # Разделяем слова запроса и объединяем их с помощью %20
     query_words = query.split()
-
     # Формируем URL для запроса в Google
-    url = f"https://www.google.com/search?q={' '.join(query_words)}"
+    url = f"https://www.google.com/search?q={'%20'.join(query_words)}"
 
     # Отправляем GET-запрос и получаем HTML-страницу с результатами поиска
-    response = requests.get(url)
-    response.raise_for_status()
+    try:
+        response = requests.get(url,headers={'User-Agent': UserAgent().googlechrome})
+        response.raise_for_status()
+        print(UserAgent().googlechrome)
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при выполнении запроса: {e}")
+        return []
 
     # Используем BeautifulSoup для парсинга HTML-страницы
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -52,21 +60,20 @@ def search_google(query, limit):
     count = 0
     for i, link in enumerate(links, start=1):
         href = link.get('href')
-        if href.startswith('/url?q='):
-            url = unquote(href[7:])
+        if href and href.startswith('/url?q='):
+            url = unquote_plus(href[7:])
             parsed_url = urlparse(url)
             cleaned_url = f"{parsed_url.scheme}://{parsed_url.netloc}{parsed_url.path}"
             try:
                 # Проверяем доступность страницы перед добавлением ссылки в результаты
-                response = requests.get(cleaned_url)
+                response = requests.get(cleaned_url,headers={'User-Agent': UserAgent().googlechrome})
                 response.raise_for_status()
                 result_links.append(cleaned_url)
                 count += 1
                 if count >= limit:
                     break
-            except requests.exceptions.HTTPError:
-                # Пропускаем ссылку, если возникает ошибка HTTP
-                continue
+            except requests.exceptions.RequestException as e:
+                print(f"Ошибка при обработке ссылки: {e}")
 
     # Добавляем задержку в 1 секунду перед каждым запросом
     time.sleep(1)
@@ -76,14 +83,18 @@ def search_google(query, limit):
 
 def save_page_content(url, filename):
     # Отправляем GET-запрос и получаем HTML-страницу
-    response = requests.get(url)
-    response.raise_for_status()
+    try:
+        response = requests.get(url,headers={'User-Agent': UserAgent().googlechrome})
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при выполнении запроса: {e}")
+        return
 
     # Используем BeautifulSoup для парсинга HTML-страницы
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Извлекаем текст с веб-страницы и удаляем разрывы
-    text = soup.get_text().replace('\n', '')
+    # Извлекаем текст с веб-страницы и удаляем лишние разрывы
+    text = soup.get_text().replace('\n\n', ' ')
 
     # Сохраняем текст в текстовый файл
     with open(filename, 'w', encoding='utf-8') as file:
@@ -91,8 +102,9 @@ def save_page_content(url, filename):
 
 
 # Пример использования функции
-user_query = 'привет'
-search_results = search_google(user_query, limit=3)
+user_query = 'двенадцати струнная гитара что это такое?'
+search_results = search_google(user_query, limit=2)
+print(search_results)
 for i, link in enumerate(search_results, start=1):
     filename = f"page_{i}.txt"
     save_page_content(link, filename)
