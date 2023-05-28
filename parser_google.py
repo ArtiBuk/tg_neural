74d46a471a96868ec4efea2de539f7a074039c43
artibuk <artibuk1052@icloud.com>
2023-05-28 16:44:28 +0700

    parser_fix

--- parser_google.py
+++ parser_google.py
@@ -6,14 +6,10 @@ from urllib.parse import unquote_plus, urlparse
 from fake_useragent import UserAgent
 import re
 import json
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer
-from sumy.nlp.stemmers import Stemmer
 from nltk.corpus import stopwords
 
 LANGUAGE = "russian"
-stemmer = Stemmer(LANGUAGE)
+
 def search_google(query, limit):
     # Разделяем слова запроса и объединяем их с помощью %20
     query_words = query.split()
@@ -88,7 +84,7 @@ def remove_extra_spaces(text):
 
 def process_text(text):
     # Удаление всех латинских символов
-    text = re.sub(r'[a-zA-Z]', '', text)
+    # text = re.sub(r'[a-zA-Z]', '', text)
     # Приведение всех слов к нижнему регистру
     text = text.lower()
     text = remove_links(text)
@@ -98,26 +94,14 @@ def process_text(text):
     return text
 
 
-def summarize_text(text, sentences_count):
-    parser = PlaintextParser.from_string(text, Tokenizer(LANGUAGE))
-    summarizer = LsaSummarizer(stemmer)
-    summarizer.stop_words = set(stopwords.words('russian'))
-
-    summary = []
-    for sentence in summarizer(parser.document, sentences_count):
-        summary.append(str(sentence))
-
-    return ' '.join(summary)
-
-
-def save_page_content(url, filename):
+def save_page_content(url):
     # Отправляем GET-запрос и получаем HTML-страницу
     try:
         response = requests.get(url, headers={'User-Agent': UserAgent().googlechrome})
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(f"Ошибка при выполнении запроса: {e}")
-        return
+        return None
 
     # Используем BeautifulSoup для парсинга HTML-страницы
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -125,25 +109,23 @@ def save_page_content(url, filename):
     # Извлекаем текст с веб-страницы и обрабатываем его
     text = soup.get_text()
     processed_text = process_text(text)
-
-    # Создаем папку "text", если она не существует
-    text_folder = os.path.join(os.path.dirname(__file__), 'text')
-    if not os.path.exists(text_folder):
-        os.makedirs(text_folder)
-
-    # Создаем выжимку текста
-    summarized_text = summarize_text(processed_text, sentences_count=10)
-
-    # Сохраняем текст в файл
-    data = {"text": summarized_text}
-    with open(os.path.join(text_folder, filename), 'w', encoding='utf-8') as file:
-        json.dump(data, file, ensure_ascii=False)
-    print(f"Файл {filename} сохранен")
+    return processed_text
 
 
-user_query = 'значения слова привет'
-search_results = search_google(user_query, limit=4)
-print(search_results)
+user_query = 'meaning of "hellow"'
+search_results = search_google(user_query, limit=1)
+all_texts = ""
+max_word_count = 0
 for i, link in enumerate(search_results, start=1):
-    filename = f"page_{i}.json"  # изменение расширения файла
-    save_page_content(link, filename)
+    text = save_page_content(link)
+    if text:
+        all_texts += text + "\n"
+        word_count = len(text.split())
+        if word_count > max_word_count:
+            max_word_count = word_count
+
+print("All texts:")
+print(all_texts)
+print("Max word count:", max_word_count)
+
+
